8624b1276e868d1de13117f8e5a90af9197b1d30
Łukasz Kozłowski <eshokei@gmail.com>
2021-10-28 18:55:17 +0200

    upd

--- ContosoUniversity/Models/student.cs
+++ ContosoUniversity/Models/student.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [StringLength(50)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
-
-        public string Secret { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
